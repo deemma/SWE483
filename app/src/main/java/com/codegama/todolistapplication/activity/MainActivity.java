570c75f62ec4e44e7f59829175b4460544edd788
Deema <DeemaAlzaidKh@gmail.com>
2023-05-24 08:30:29 +0300

    changed homescreen ui

--- app/src/main/java/com/codegama/todolistapplication/activity/MainActivity.java
+++ app/src/main/java/com/codegama/todolistapplication/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -33,11 +34,10 @@ public class MainActivity extends BaseActivity implements CreateTaskBottomSheetF
     @BindView(R.id.taskRecycler)
     RecyclerView taskRecycler;
     @BindView(R.id.addTask)
-    TextView addTask;
+    Button addTask;
     TaskAdapter taskAdapter;
     List<Task> tasks = new ArrayList<>();
-    @BindView(R.id.noDataImage)
-    ImageView noDataImage;
+
     @BindView(R.id.calendar)
     ImageView calendar;
 
@@ -51,7 +51,7 @@ public class MainActivity extends BaseActivity implements CreateTaskBottomSheetF
         ComponentName receiver = new ComponentName(this, AlarmBroadcastReceiver.class);
         PackageManager pm = getPackageManager();
         pm.setComponentEnabledSetting(receiver, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
-        Glide.with(getApplicationContext()).load(R.drawable.first_note).into(noDataImage);
+//        Glide.with(getApplicationContext()).load(R.drawable.first_note).into(noDataImage);
 
         addTask.setOnClickListener(view -> {
             CreateTaskBottomSheetFragment createTaskBottomSheetFragment = new CreateTaskBottomSheetFragment();
@@ -86,12 +86,12 @@ public class MainActivity extends BaseActivity implements CreateTaskBottomSheetF
                 return tasks;
             }
 
-            @Override
-            protected void onPostExecute(List<Task> tasks) {
-                super.onPostExecute(tasks);
-                noDataImage.setVisibility(tasks.isEmpty() ? View.VISIBLE : View.GONE);
-                setUpAdapter();
-            }
+//            @Override
+//            protected void onPostExecute(List<Task> tasks) {
+//                super.onPostExecute(tasks);
+//                noDataImage.setVisibility(tasks.isEmpty() ? View.VISIBLE : View.GONE);
+//                setUpAdapter();
+//            }
         }
 
         GetSavedTasks savedTasks = new GetSavedTasks();
